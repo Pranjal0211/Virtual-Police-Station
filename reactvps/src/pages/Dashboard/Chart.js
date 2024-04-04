57d4ef071c39c9fd675e22e2571c0e8935083e01
Pranjal0211 <pranjalsharma0211@gmail.com>
2024-04-04 11:00:00 -0700

    adding chart

---
+++ reactvps/src/pages/Dashboard/Chart.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
+
+import Nav from '../../components/Dashboard/Utils/Nav'
+import SideBar from '../../components/Dashboard/Utils/SideBar'
+import Chart from '../../components/Dashboard/Chart/Chart'
+
+import Auth from '../../middlewares/Auth'
+
+function Dashboard() {
+    const [sideBarOpen, setSideBarOpen] = useState(false)
+
+    const [status, setStatus] = useState({})
+    useEffect(() => {
+        console.log('Mounting');
+        Axios.get('/reports/info/count')
+            .then((res) => {
+                if (res.data.status === 'success') {
+                    console.log(res.data);
+                    setStatus(res.data.count)
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }, [])
+
+    return (
+        <div className="flex h-screen bg-gray-200 font-roboto">
+
+            <div
+                onClick={() => { setSideBarOpen(false) }}
+                className={`fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden ${sideBarOpen ? 'block' : 'hidden'}`}>
+            </div>
+
+            <SideBar sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
+
+            <div className="flex-1 flex flex-col overflow-hidden">
+                <Nav sideBarOpen={sideBarOpen} setSideBarOpen={setSideBarOpen} />
+                <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+                    <Chart status={status} />
+                </main>
+            </div>
+
+        </div >
+    )
+}
+
+export default Auth(Dashboard)
